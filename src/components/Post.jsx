16803c77386daac788a2fd30898a6a261b2d602d
Aman <ak193566@gmail.com>
2023-11-22 18:01:03 +0530

    add some more features

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Post({
+    src,
+    alt,
+    id,
+    dblClickEvent
+}) {
+  return (
+    <>
+      <div className="post">
+        <img className={id} src={src.dp} alt={alt} onDoubleClick={dblClickEvent} />
+        <i className="ri-heart-3-fill"></i>
+      </div>
+      <div className="icons">
+        {/* <i className="ri-heart-line" id='heart'></i> */}
+        <i className="ri-heart-fill" id="heart"></i>
+        <i className="ri-chat-3-line"></i>
+        <i className="ri-share-line"></i>
+      </div>
+      <div className="feeds">
+        <h5>{src.like}</h5>
+        <p>
+          {src.text}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Post
